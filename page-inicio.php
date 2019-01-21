444c05776d3ac01eff3fefa8aede8e55b9793792
cesar zuñiga <cesarzv1997@gmail.com>
2019-01-21 10:11:03 -0600

    'Entregra1'

---
+++ page-inicio.php
@@ -0,0 +1,48 @@
+<?php
+	get_header();
+?>
+	<section><div class="logoContacto" id="logoHome"></div></section>
+	<section class="seccionHome1">
+		
+			<div class="tituloSeccion1Home"><p>TE AYUDAMOS A CUIDARTE</p><p>Todo lo que necesitas <br>para cuidar tu salud</p></div>
+			<div id="imagen1" class="imagen1Home">
+				<div class="verMas"><p><u>Nutricionista</p></u><a href="#"><p>Ver más</p></a></div>
+				
+			</div>
+			<div id="imagen2Home" class="imagen1Home">
+				<div class="verMas"><u><p>Deportes</p></u><a href="#"><p>Ver más</p></a></div>
+				
+			</div><div></div>
+			<div id="imagen3Home" class="imagen1Home">
+				<div class="verMas"><u><p>Entrenamiento</p></u><a href="#"><p>Ver más</p></a></div>
+				
+			</div><div></div>
+			<div id="imagen4Home" class="imagen1Home">
+				<div class="verMas"><u><p>Implementos</p></u><a href="#"><p>Ver más</p></a></div>
+				
+			</div><div></div>
+			</div>
+			
+		
+	</section>
+	<section class="seccionHome2">
+		<div class="tituloSeccion2Home"><p>PROMOCIONES</p></div>
+		<div class="">
+			<button type="submit" class="anterior"></button>
+		</div><div class="promociones"></div><div class="">
+			<button type="submit" class="siguiente"></button>
+		</div>
+		
+	</section>
+
+	<section class="seccionHome3">
+		<div class="tituloSeccion3Home"><p>RESULTADOS</p><p>Consultá algunos de los<br>resultados más importantes<br>a nivel munidal</p></div>
+		<button id="anteriorSeccion3" type="submit" class="anterior"></button><div class="resultados"></div><div class="textoAsociados"><p>Echa un vistazo a los <br>
+precios especiales en<br>
+diferentes gimnasios
+</p></div><button id="siguienteSeccion3" type="submit" class="siguiente"></button>
+		
+	</section>
+<?php
+	get_footer();
+?>
